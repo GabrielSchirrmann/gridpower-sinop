1d9bb7d989b30a3c7d57152f2c649934aa29a939
Gabriel Schirrmann Faria <gabrielschirrmannfarias@gmail.com>
2026-06-26 15:58:36 -0400

    refactor: slim down main.c, delegate to menu module

--- src/main.c
+++ src/main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include "grafo.h"
-#include "dijkstra.h"
-#include "bfs.h"
+#include "menu.h"
 
-int main(void) {
-    Grafo* g = ler_grafo("data/rede_sinop.csv");
+int main(int argc, char** argv) {
+    const char* caminho = (argc > 1) ? argv[1] : "data/rede_sinop.csv";
+
+    Grafo* g = ler_grafo(caminho);
     if (g == NULL) {
         return 1;
     }
 
-    imprimir_grafo(g);
-
-    dijkstra(g, 0);
-
-    bfs_conectividade(g, 0, -1, -1);   // rede intacta
-    bfs_conectividade(g, 0, 3, 4);     // cai a linha 3--4
-    bfs_conectividade(g, 0, 0, 3);     // cai a linha 0--3 (no ciclo)
+    executar_menu(g);
 
     liberar_grafo(g);
     return 0;
